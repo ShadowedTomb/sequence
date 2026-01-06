c64f5ae0fba925a84c014568e9a286f5265552d6
ShadowedTomb <chamelisarkar235@gmail.com>
2026-01-07 02:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import os
 from os import environ
 
-TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "756596:AAF")
-APP_ID = int(os.environ.get("APP_ID", "2355"))
-API_HASH = os.environ.get("API_HASH", "4vsavkjahvsdhk")
-OWNER_ID = int(os.environ.get("OWNER_ID", "5741918628"))
+TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", " ")
+APP_ID = int(os.environ.get("APP_ID", " "))
+API_HASH = os.environ.get("API_HASH", " ")
+OWNER_ID = int(os.environ.get("OWNER_ID", " "))
 PORT = os.environ.get("PORT", "8080")
-DB_URL = os.environ.get("DB_URI", "mongodb+srv://odb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "Rex_sequencebott")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "S_QV_Sbot")
-FSUB_PIC = os.environ.get("FSUB_PIC", "https://ibb.co/FL66q5G9")
-START_PIC =os.environ.get("START_PIC", "https://ibb.co/FL66q5G9")
-START_MSG = os.environ.get("START_MSG", "<b>Bᴀᴋᴀᴀᴀ...!!!{mention}</b> \n<blockquote><b><i>Iᴀᴍ ᴀ ᴀᴅᴠᴀɴᴄᴇ sᴇǫᴜᴇɴᴄᴇ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴅᴠᴀɴᴄᴇ ғᴇᴀᴛᴜʀᴇs. I ᴄᴀɴ sᴇǫᴜᴇɴᴄᴇ ʏᴏᴜʀ ғɪʟᴇs ᴇᴀsɪʟʏ ɪɴ ᴀ sᴇᴄᴏɴᴅ...!!</i></b></blockquote>")
-ABOUT_TXT = os.environ.get("ABOUT_MESSAGE", "<i><b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/RexBots_Official>RexBots</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/RexBots_Official>RexBOTS</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/about_zani/117'>ZANI</a>\n◈ ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>ᴍᴏɴɢᴏ ᴅʙ</a>\n» ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/about_zani/117'>ZANI</a></blockquote></b></i>")
-HELP_TXT =  os.environ.get("HELP_MESSAGE", "⁉️ Hᴇʟʟᴏ {mention} \n<blockquote expandable><b><i>➪ Iᴀᴍ ᴀ ᴘᴜʙʟɪᴄ ғɪʟᴇ(s) sᴇǫᴜᴇɴᴄᴇ ʙᴏᴛ I ᴄᴀɴ sᴇǫᴜᴇɴᴄᴇ ᴛʜᴇ ғɪʟᴇs ᴀɴᴅ ᴀʟsᴏ I ᴄᴀɴ sᴇɴᴅ ᴛʜᴀᴛ ғɪʟᴇs ɪɴ ᴅᴜᴍᴘ ᴄʜᴀɴɴᴇʟ. </i></b></blockquote>")
+DB_URL = os.environ.get("DB_URI", " ")
+DB_NAME = os.environ.get("DB_NAME", "ShadowedTomb")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", " ")
+FSUB_PIC = os.environ.get("FSUB_PIC", " ")
+START_PIC =os.environ.get("START_PIC", " ")
+START_MSG = os.environ.get("START_MSG", "<b>Sᴇɴᴘᴀɪ...!!!{mention}</b> \n<blockquote><b><i>Exᴘᴇʀɪᴇɴᴄᴇ ᴛʜᴇ ꜰᴀꜱᴛᴇꜱᴛ ᴡᴀʏ ᴛᴏ ᴏʀɢᴀɴɪᴢᴇ ʏᴏᴜʀ ᴄᴏɴᴛᴇɴᴛ. ɪ ꜱᴘᴇᴄɪᴀʟɪᴢᴇ ɪɴ ꜱᴇᴀᴍʟᴇꜱꜱ ꜰɪʟᴇ ꜱᴇQᴜᴇɴᴄɪɴɢ. ᴇꜰꜰɪᴄɪᴇɴᴄʏ, ʀᴇᴅᴇꜰɪɴᴇᴅ.</i></b></blockquote>")
+ABOUT_TXT = os.environ.get("ABOUT_MESSAGE", "<i><b><blockquote>✧ ᴄʜᴀɴɴᴇʟ: <a href=https://t.me/ShadowedTomb>ꜱʜᴀᴅᴏᴡᴇᴅ ᴛᴏᴍʙ</a>\n✧ ᴏᴡɴᴇʀ: <a href=https://t.me/ShadowedTomb>ꜱʜᴀᴅᴏᴡᴇᴅ ᴛᴏᴍʙ</a>\n✧ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/name_huh'>ᴺᴬᴹᴱ ᴴᵁᴴ</a>\n✧ ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>ᴍᴏɴɢᴏ </a></blockquote></b></i>")
+HELP_TXT =  os.environ.get("HELP_MESSAGE", "ᴀʀᴀ.. ᴀʀᴀ... {mention} Sᴇɴᴘᴀɪ \n<blockquote expandable><b><i>ɪ'ᴍ ᴀ ꜰɪʟᴇ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ ᴅᴇꜱɪɢɴᴇᴅ ꜰᴏʀ ᴘᴜʙʟɪᴄ ᴜꜱᴇ. ɪ ᴄᴀɴ ᴏʀɢᴀɴɪᴢᴇ ᴀɴᴅ ꜱᴇQᴜᴇɴᴄᴇ ʏᴏᴜʀ ꜰɪʟᴇꜱ, ᴀɴᴅ ᴇᴠᴇɴ ᴛʀᴀɴꜱꜰᴇʀ ᴛʜᴇᴍ ᴛᴏ ᴀ ᴅᴇꜱɪɢɴᴀᴛᴇᴅ ᴅᴜᴍᴘ ᴄʜᴀɴɴᴇʟ.. </i></b></blockquote>")
 TG_BOT_WORKERS = 10000
 FSUB_LINK_EXPIRY = 300
-DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-10654657458"))
-LOG_FILE_NAME = "links-sharingbot.txt"
+DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", " "))
+LOG_FILE_NAME = "sequence-bot.log"
 SEASON_PATTERN = r'[Ss](\d{1,2})'
 EPISODE_PATTERN = r'[Ee][Pp]?(\d{1,3})'
 QUALITY_PATTERN = r'(480p|720p|1080p|HDRip|2k|4k)'
